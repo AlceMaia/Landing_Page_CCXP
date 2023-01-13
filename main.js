8dd55f5ee890706600fb3d7a791892d24961cb33
AlceMaia <alceniziamaia@gmail.com>
2023-01-12 21:34:09 -0300

    Melhorando a experiência do usuário

--- main.js
+++ main.js
@@ -42,5 +42,55 @@ function addRedBorder(id)
 function highlightCard(selector)
 {
     var element = document.querySelector(selector);
-    element.classList.toggle("card-highligh");
+    element.classList.toggle("card-highlight");
 }
+
+function checkKeyboardCode()
+{
+    document.addEventListener('keydown', (event) => {
+        var name = event.key;
+        var code = event.code;
+        //alert the name and key code on keydown
+        alert(`Tecla pressionada ${name} \r\n Key code: ${code}`);
+    }, false);
+        
+}
+
+function addKeyboardEventListeners()
+{
+    document.addKeyboardEventListener('keydown', (event) => {
+
+        var ingresso1 = document.getElementById("quinta");
+        var ingresso2 = document.getElementById("sexta");
+        var ingresso3 = document.getElementById("sabado");
+        var ingresso4 = document.getElementById("domingo");
+
+        var code = event.code;
+        if(code == 'Numpad1') {
+            ingresso1.classList.toggle("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.removo("card-highlight");
+        }
+        if(code == 'Numpad2') {
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.toggle("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.remove("card-highlight");
+        }
+        if(code == 'Numpad3') {
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.toggle("card-highlight");
+            ingresso4.classList.remove("card-highlight");
+        }
+        if(code == 'Numpad4') {
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.toggle("card-highlight");
+        }
+    }, false);
+}
+//checkKeyboardCode();
+addKeyboardEventListeners();
